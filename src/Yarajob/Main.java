5c11caf32fb80a484695195f1f447d20195c89ee
HUsefi <hyousefi412@gmail.com>
2018-07-28 20:46:32 +0430

    First Java project

--- src/Yarajob/Main.java
+++ src/Yarajob/Main.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Main {
 
 
- //  private static Object[] listOfObject;
+    private Object[] listOfObject;
 
     public static void main(String args[]) {
-//          createObject();
- //         writeToFile();
-        String   input;
+
+
+        String input;
         Scanner scanner = new Scanner(System.in);
         List<Object> listOfObject = new ArrayList<>();
 
@@ -62,8 +63,11 @@ public class Main {
                 phoneProgrammer.setNumberReleasedApp(Integer.parseInt(scanner.nextLine()));
                 listOfObject.add(phoneProgrammer);
             }
-        }while(!input.equals("4"));
-        Writer writer = null;
+        } while (!input.equals("4"));
+
+
+
+        Writer writer=null;
         for(Object obj : listOfObject) {
             try {
                 writer = new BufferedWriter(new OutputStreamWriter(
@@ -80,24 +84,36 @@ public class Main {
             }
         }
 
+
+        String line = null;
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader =
+                    new FileReader("List.txt");
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader =
+                    new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // Always close files.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            "List.txt" + "'");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + "List.txt" + "'");
+
         }
 
+    }
 
-
-
-
-
-//            for(Object obj : listOfObject){
-//            if(obj instanceof Accountant){
-//                Accountant d1 = (Accountant) obj;
-//                System.out.println(d1.toString());
-//            } else if (obj instanceof WebProgrammer){
-//                WebProgrammer w1=(WebProgrammer) obj;
-//                System.out.println(w1.toString());
-//            }
-//            else if (obj instanceof PhoneProgrammer){
-//                PhoneProgrammer p1=(PhoneProgrammer) obj;
-//                System.out.println(p1.toString());
-//            }
-//        }
 }
